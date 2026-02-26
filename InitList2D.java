5dd43612dd416285548d1f6796b320a69f1628f2
jonaskaun <jonas.kaun@crpusd.org>
2026-02-26 08:51:41 -0800

    Add files via upload

---
+++ InitList2D.java
@@ -0,0 +1,14 @@
+/*
+ * Activity 3.3.1
+ */
+public class InitList2D
+{
+  public static void main(String[] args)
+  {
+    String[][] board = { {"close", "quarter"},
+                        {"moon", "rock"},
+                        {"band", "stand"},
+                        {"out", "shine"}  }; 
+
+ }
+}
